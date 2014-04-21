e894d40429619e351f175b1a3d6bd7b4d4123e70
Zoltan Biber <zoltan.biber@gmail.com>
2014-04-21 18:20:40 +0100

    Pulp Fiction with 7 simple pages plus one CSS file

--- test/dom_test.rb
+++ test/dom_test.rb
@@ -7,77 +7,79 @@ class DomTest < Test::Unit::TestCase
 		@html = @dom.elements.first
 		@head = @dom.elements.last.children.first
 		@body = @dom.elements.last.children.last
+
 	end
 
 	def test_document_is_html_doctype
-		assert_equal ___, @dom.children.first.name
+		assert_equal "html", @dom.children.first.name
 	end
 
 	def test_first_element_is_the_html_root
-		assert_equal ___, @dom.elements.first.name
+		assert_equal "html", @dom.elements.first.name
 	end
 
 	def test_html_has_a_head_and_a_body
-		assert_equal ___, @html.children.length
-		assert_equal ___, @html.children.first.name
-		assert_equal ___, @html.children.last.name
+		assert_equal 2, @html.children.length
+		assert_equal "head", @html.children.first.name
+		assert_equal "body", @html.children.last.name
 	end
 
 	def test_document_has_a_title
-		assert_equal ___, @dom.title
+		assert_equal "My Amazing Webpage", @dom.title
 	end
 
 	def test_document_has_text_nodes_that_maintain_whitespace
-		assert_equal ___, @body.children.length # Might not be what you expect!
-		assert_equal ___, @body.children.first.is_a?(Nokogiri::XML::Text)
-		assert_equal ___, @body.children.first.content.strip
+		assert_equal 9, @body.children.length # Might not be what you expect!
+		assert_equal true, @body.children.first.is_a?(Nokogiri::XML::Text)
+		assert_equal "", @body.children.first.content.strip
+
 	end
 
 	def test_body_has_child_elements
-		assert_equal ___, @body.elements.length
-		assert_equal ___, @body.elements.map {|e| e.name }
+		assert_equal 5, @body.elements.length
+		assert_equal ["h1", "img", "p", "ul", "div"], @body.elements.map {|e| e.name }
 	end
 
 	def test_elements_has_content
-		assert_equal ___, @body.elements[0].content
-		assert_equal ___, @body.elements[1].content
+		assert_equal "This is an amazing webpage", @body.elements[0].content
+		assert_equal "", @body.elements[1].content
 	end
 
 	def test_elements_have_sub_elements
 		@ul = @body.elements[3]
-		assert_equal ___, @ul.elements.length
-		assert_equal ___, @ul.elements[0].content
-		assert_equal ___, @ul.elements[1].content
-		assert_equal ___, @ul.elements[2].content
+		assert_equal 3, @ul.elements.length
+		assert_equal "Interesting fact", @ul.elements[0].content
+		assert_equal "Hard-hitting point", @ul.elements[1].content
+		assert_equal "Superb insight", @ul.elements[2].content
 	end
 
 	def test_elements_can_have_attributes
 		@img = @body.elements[1]
-		assert_equal ___, @img.attributes.length
-		assert_equal ___, @img.attributes.keys
-		assert_equal ___, @img.attr(:src)
-		assert_equal ___, @img.attr(:alt)
+		assert_equal 2, @img.attributes.length
+		assert_equal ["src", "alt"], @img.attributes.keys
+		assert_equal "img/ulysses31_crew.jpg", @img.attr(:src)
+		assert_equal "Ulysses 31", @img.attr(:alt)
 	end
 
 	def test_elements_can_be_found_by_id
 		content = @dom.css('#content').first
-		assert_equal ___, content.name
-		assert_equal ___, content.elements.length
+		assert_equal "div", content.name
+		assert_equal 2, content.elements.length
 	end
 
 	def test_elements_can_be_found_by_class
 		facts = @dom.css('.fact')
-		assert_equal ___, facts.length
-		assert_equal ___, facts.first.attr(:class)
-		assert_equal ___, facts.first.content
+		assert_equal 3, facts.length
+		assert_equal "fact", facts.first.attr(:class)
+		assert_equal "Interesting fact", facts.first.content
 	end
 
 	def test_dom_can_be_changed
 		@img = @body.elements[1]
 		@img.set_attribute("src", "http://placekitten.com/200/300")
 		@img.set_attribute("alt", "Now I am a kitten")
-		assert_equal ___, @img.attr(:src)
-		assert_equal ___, @img.attr(:alt)
+		assert_equal "http://placekitten.com/200/300", @img.attr(:src)
+		assert_equal "Now I am a kitten", @img.attr(:alt)
 	end
 
 	def test_dom_can_have_elements_added
@@ -85,16 +87,17 @@ class DomTest < Test::Unit::TestCase
 		fun_fact.content = "I rode an elephant when I was 5"
 		@ul = @body.elements[3]
 		@ul.add_child(fun_fact)
-
-		assert_equal ___, @ul.elements.length
-		assert_equal ___, @ul.elements.last.content
+		assert_equal 4, @ul.elements.length
+		assert_equal "I rode an elephant when I was 5", @ul.elements.last.content
 	end
 
 	def test_dom_elements_can_be_moved
 		lead = @dom.css('.lead').first
 		lead.parent = @body
-		assert_equal ___, @body.elements.length
-		assert_equal ___, @body.elements.map {|e| e.name }
+binding.pry
+
+		assert_equal 6, @body.elements.length
+		assert_equal ["h1", "img", "p", "ul", "div", "p"], @body.elements.map {|e| e.name }
 	end
 
 end
